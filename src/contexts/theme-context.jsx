4cc62a309a17f1c53228dcf7e43a63ac2f58b410
Elaini Carlete <elainicarletethomazelli3@gmail.com>
2024-02-16 21:15:55 -0300

    modified color

--- src/contexts/theme-context.jsx
+++ src/contexts/theme-context.jsx
@@ -7,7 +7,7 @@ export const themes = {
         boxShadow: '2px 2px 10px 5px #D87093'
     },
     dark: {
-        background: '#6d6d6d',
+        background: '#dddddd',
         color: '#000000',
         boxShadow: '2px 2px 10px 5px #000000'
     },
