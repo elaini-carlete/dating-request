859242024ea82e4b34fc9d9a26894bbed80c71cb
Elaini Carlete <elainicarletethomazelli3@gmail.com>
2024-02-20 18:45:58 -0300

    image of the theme change button working

--- src/components/Theme-Toggler-Button/theme-toggler-button.jsx
+++ src/components/Theme-Toggler-Button/theme-toggler-button.jsx
@@ -1,13 +1,25 @@
 import React, { useContext } from "react"
 import { ThemeContext, themes } from "../../contexts/theme-context"
 import { Button } from "../Button/button"
+import cherryBlossomsLight from "../images/cherry-blossoms-light.png";
+import cherryBlossomsDark from "../images/cherry-blossoms-dark.png";
 
 export const ThemeTogglerButton = () => {
     const { theme, setTheme } = useContext(ThemeContext)
 
     return(
-        <div style={{ color: theme.color, background: theme.background }} className="theme-btn">
-            <Button onClick={() => setTheme(theme === themes.light ? themes.dark : themes.light)}></Button>
+        <div style={{ color: theme.color, background: theme.background}} className="theme-btn">
+            <Button
+                onClick={() => setTheme(theme === themes.light ? themes.dark : themes.light)}>
+                    <img
+                        src={
+                            theme === themes.light
+                            ? cherryBlossomsLight
+                            : cherryBlossomsDark
+                        }
+                        alt="image-btn-themes"
+                    />
+            </Button>
         </div>
     )
 }
--- src/contexts/theme-context.jsx
+++ src/contexts/theme-context.jsx
@@ -1,19 +1,15 @@
 import { createContext, useState } from "react";
-import cherryBlossomsLight from "../components/images/cherry-blossoms-light.png";
-import cherryBlossomsDark from "../components/images/cherry-blossoms-dark.png";
 
 export const themes = {
     light: {
         background: '#93a5d4',
         color: '#d87093',
-        boxShadow: '2px 2px 10px 5px #D87093',
-        src: {cherryBlossomsLight}
+        boxShadow: '2px 2px 10px 5px #D87093'
     },
     dark: {
         background: '#2f3042',
         color: '#000000',
-        boxShadow: '2px 2px 10px 5px #000000',
-        src: {cherryBlossomsDark}
+        boxShadow: '2px 2px 10px 5px #000000'
     },
 };
 
